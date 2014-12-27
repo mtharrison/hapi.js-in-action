829aad7b0efb79c6c83b1613a40d17491635d471
Matt Harrison <hi@matt-harrison.com>
2014-12-27 23:42:24 +0000

    2.4.3

--- CH2 - Building an API/2.4/2.4.3/index.js
+++ CH2 - Building an API/2.4/2.4.3/index.js
@@ -1,51 +1,76 @@
 var Hapi = require('hapi');
+var mysql = require('mysql');
 var recipes = require('./recipes');
 
 var server = new Hapi.Server();
 server.connection({port: 4000});
 
-server.method('search', function(query, next) {
-    var results = [];
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'dindin'
+});
 
-    if (query.cuisine) {
-        for(var i in recipes) {
-            var recipe = recipes[i];
+// Server methods
 
-            if (recipe.cuisine === query.cuisine) {
-                results.push(recipe);
-            }
-        }
-    } else {
-        results = recipes;
+server.method('search', function (cuisine, next) {
+
+    var sql = 'SELECT * FROM recipes';
+    var sqlParams = [];
+
+    if (cuisine) {
+        sql += ' WHERE cuisine = ?';
+        sqlParams = [cuisine];
     }
 
-    next(null, results);
-
+    connection.query(sql, sqlParams, function (err, results) {
+        next(err, results);
+    });
 }, {});
+
+server.method('retrieve', function (id, next) {
+    connection.query('SELECT * FROM recipes WHERE id = ?', [id], function (err, results) {
+        next(err, results ? results[0] : null);       
+    });
+}, {});
+
+// Routing
 
 server.route([{
     method: 'GET',
     path: '/recipes',
-    handler: function(request, reply) {
-        server.methods.search(request.query, function(err, results){
+    handler: function (request, reply) {
+        
+        server.methods.search(request.query.cuisine, function (err, results) {
+
+            if (err) {
+                throw err;
+            }
+
             reply(results);
         });
     }
 }, {
     method: 'GET',
     path: '/recipes/{id}',
-    handler: function(request, reply) {
+    handler: function (request, reply) {
 
-        var id = request.params.id;
+        server.methods.retrieve(request.params.id, function (err, recipe) {
 
-        if (recipes.hasOwnProperty(id)) {
-            reply(recipes[id]);
-        } else {
-            reply("Recipe not found").code(404);
-        }
+            if (err) {
+                throw err;
+            }
+            
+            if (recipe) {
+                reply(recipe);
+            } else {
+                reply('Recipe not found').code(404);
+            }
+        });
     }
 }]);
 
-server.start(function(){
+server.start(function () {
     console.log('Server listening at:', server.info.uri);
 });
