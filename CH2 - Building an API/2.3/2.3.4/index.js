34b339ea51ce020834d0abf5b7f6cbce6a78189d
Matt Harrison <hi@matt-harrison.com>
2014-12-27 16:57:26 +0000

    Fixing numbering of CH2 code segments

---
+++ CH2 - Building an API/2.3/2.3.4/index.js
@@ -0,0 +1,46 @@
+var Hapi = require('hapi');
+var recipes = require('./recipes');
+
+var server = new Hapi.Server();
+server.connection({port: 4000});
+
+server.route([{
+    method: 'GET',
+    path: '/recipes',
+    handler: function(request, reply) {
+
+        var results = [];
+
+        if (request.query.cuisine) {
+            for(var i in recipes) {
+                var recipe = recipes[i];
+
+                if (recipe.cuisine === request.query.cuisine) {
+                    results.push(recipe);
+                }
+            }
+        } else {
+            results = recipes;
+        }
+
+        reply(results);
+
+    }
+}, {
+    method: 'GET',
+    path: '/recipes/{id}',
+    handler: function(request, reply) {
+
+        var id = request.params.id;
+
+        if (recipes.hasOwnProperty(id)) {
+            reply(recipes[id]);
+        } else {
+            reply("Recipe not found").code(404);
+        }
+    }
+}]);
+
+server.start(function(){
+    console.log('Server listening at:', server.info.uri);
+});
