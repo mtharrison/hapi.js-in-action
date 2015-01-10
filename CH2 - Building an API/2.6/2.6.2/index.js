04739f238ffbe2faf82ceb4a18aa88d08c05d836
Matt Harrison <hi@matt-harrison.com>
2015-01-10 20:46:04 +0000

    Code for 2.6.2

---
+++ CH2 - Building an API/2.6/2.6.2/index.js
@@ -0,0 +1,106 @@
+var Hapi = require('hapi');
+var mysql = require('mysql');
+var Bcrypt = require('bcrypt');
+
+var server = new Hapi.Server();
+server.connection({port: 4000});
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dindin'
+});
+
+// Server methods
+
+server.method('search', function (cuisine, next) {
+
+    var sql = 'SELECT * FROM recipes';
+    var sqlParams = [];
+
+    if (cuisine) {
+        sql += ' WHERE cuisine = ?';
+        sqlParams = [cuisine];
+    }
+
+    connection.query(sql, sqlParams, function (err, results) {
+        next(err, results);
+    });
+}, {});
+
+server.method('retrieve', function (id, next) {
+    connection.query('SELECT * FROM recipes WHERE id = ?', [id], function (err, results) {
+        next(err, results ? results[0] : null);       
+    });
+}, {
+    cache: {
+        expiresIn: 60 * 1000
+    }
+});
+
+server.method('create', function (recipe, next) {
+    
+    var sql = 'INSERT INTO recipes (name, cooking_time, prep_time, serves, cuisine, ingredients, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)';
+
+    connection.query(sql, 
+        [
+            recipe.name,
+            recipe.cooking_time,
+            recipe.prep_time,
+            recipe.serves,
+            recipe.cuisine,
+            recipe.ingredients,
+            recipe.user_id,
+        ], 
+        function (err, results) {
+            next(err, results);
+        });
+}, {});
+
+server.method('star', function (id, next) {
+
+    var sql = 'UPDATE recipes SET stars = stars + 1 WHERE id = ?';
+
+    connection.query(sql, [id], function (err, results) {
+        next(err, results);
+    });
+}, {});
+
+// Plugin registration
+
+server.register(require('hapi-auth-basic'), function (err) {
+
+    if (err) {
+        throw err;
+    }
+
+    server.auth.strategy('api', 'basic', { 
+        validateFunc: function (username, password, callback) {
+
+            connection.query('SELECT * FROM users WHERE username = ?', [username], function (err, results) {
+
+                if (err) {
+                    return callback(err, false);
+                }
+
+                if (results.length !== 1) {
+                    return callback(null, false);                    
+                }
+
+                var user = results[0];
+
+                Bcrypt.compare(password, user.password, function (err, isValid) {
+                    callback(err, isValid, { id: user.id, username: user.username });
+                });
+
+            });
+        }
+    });
+
+    server.route(require('./routes'));
+
+    server.start(function () {
+        console.log('Server listening at:', server.info.uri);
+    });
+});
---
+++ CH2 - Building an API/2.6/2.6.2/routes.js
@@ -0,0 +1,66 @@
+module.exports = [{
+    method: 'GET',
+    path: '/recipes',
+    config: {
+        auth: 'api'
+    },
+    handler: function (request, reply) {
+        request.server.methods.search(request.query.cuisine, function (err, results) {
+
+            if (err) {
+                throw err;
+            }
+
+            reply(results);
+        });
+    }
+}, {
+    method: 'GET',
+    path: '/recipes/{id}',
+    handler: function (request, reply) {
+        request.server.methods.retrieve(request.params.id, function (err, recipe) {
+
+            if (err) {
+                throw err;
+            }
+            
+            if (recipe) {
+                reply(recipe);
+            } else {
+                reply('Recipe not found').code(404);
+            }
+        });
+    }
+}, {
+    method: 'POST',
+    path: '/recipes',
+    config: {
+        payload: {
+            parse: true,
+            output: 'data'
+        }
+    },
+    handler: function (request, reply) {
+        request.server.methods.create(request.payload, function (err, results){
+
+            if(err) {
+                throw err;
+            }
+
+            reply({status: 'ok'});
+        });
+    }
+}, {
+    method: 'POST',
+    path: '/recipes/{id}/star',
+    handler: function (request, reply) {
+        request.server.methods.star(request.params.id, 1, function (err, results){
+
+            if(err) {
+                throw err;
+            }
+
+            reply({status: 'ok'});
+        });
+    }
+}];
