4dd21faabe0d0bb6132baf68c3d6d33358d2f0ae
Matt Harrison <hi@matt-harrison.com>
2015-01-21 22:21:00 +0000

    3.2

--- CH3 - Building a Website/3.2/index.js
+++ CH3 - Building a Website/3.2/index.js
@@ -10,6 +10,22 @@ server.register(require('dindin-api'),
         throw err;
     }
 
+    server.route([{
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./public/index.html');
+        }
+    }, {
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: 'public'
+            }
+        }
+    }]);
+
     server.start(function () {
         console.log('Started server at', server.info.uri);
     });
