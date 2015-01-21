e13b656a3a345d70203928056468be21fe69b3be
Matt Harrison <hi@matt-harrison.com>
2015-01-21 20:54:41 +0000

    Tabs -> spaces

--- CH3 - Building a Website/3.2/index.js
+++ CH3 - Building a Website/3.2/index.js
@@ -4,13 +4,13 @@ var server = new Hapi.Server();
 server.connection({port: 4000});
 
 server.register(require('dindin-api'),
-	function (err) {
+    function (err) {
 
-	if (err) {
-		throw err;
-	}
+    if (err) {
+        throw err;
+    }
 
-	server.start(function () {
-		console.log('Started server at', server.info.uri);
-	});
+    server.start(function () {
+        console.log('Started server at', server.info.uri);
+    });
 });
