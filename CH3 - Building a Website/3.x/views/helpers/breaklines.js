6d3a9388ca83869ed7bdb53392603437f594d27a
Matt Harrison <hi@matt-harrison.com>
2015-02-20 13:25:38 +0000

    Removed 3.x

--- CH3 - Building a Website/3.x/views/helpers/breaklines.js
+++
@@ -1,7 +0,0 @@
-var Handlebars = require('handlebars');
-
-module.exports = function(text) {
-    text = Handlebars.Utils.escapeExpression(text);
-    text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
-    return new Handlebars.SafeString(text);
-};
