a44e9874f70d613f9ff4a6fa62f60c5dd23616d7
Matt Harrison <hi@matt-harrison.com>
2015-06-14 16:22:34 +0800

    Use handler format in pre function

--- CH4 - Routes and Handlers in Depth/4.4/4.4.3/index.js
+++ CH4 - Routes and Handlers in Depth/4.4/4.4.3/index.js
@@ -4,25 +4,25 @@ var Fs = require('fs');
 var server = new Hapi.Server();
 server.connection({ port: 4000 });
 
-server.method('poem', function (next) {
+server.method('poem', function (request, reply) {
 
     Fs.readFile('./poem.txt', function (err, data) {
 
-        next(data.toString());
+        reply(data.toString());
     });
 });
 
 server.route({
+    config: {
+        pre: [
+            'poem'
+        ]
+    },
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
 
         reply(request.pre.poem);
-    },
-    config: {
-        pre: [
-            'poem()'
-        ]
     }
 });
 
