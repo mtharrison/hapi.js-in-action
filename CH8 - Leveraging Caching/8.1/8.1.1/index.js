b4b823fa930241c4a3bd597198894f7daaef6aa2
Matt Harrison <hi@matt-harrison.com>
2015-07-21 11:36:58 +0800

    Added the 8.1 subsections

---
+++ CH8 - Leveraging Caching/8.1/8.1.1/index.js
@@ -0,0 +1,20 @@
+var Hapi = require('hapi');
+var Path = require('path');
+
+var server = new Hapi.Server();
+server.connection({ port: 4000 });
+
+server.route({
+    method: 'GET',
+    path: '/image.png',
+    handler: function (request, reply) {
+
+        var response = reply.file(Path.join(__dirname, 'image.png'));
+        response.header('Cache-Control', 'max-age=86400');
+    }
+});
+
+server.start(function () {
+
+    console.log('Server running at:', server.info.uri);
+});
---
+++ CH8 - Leveraging Caching/8.1/8.1.3/index.js
@@ -0,0 +1,25 @@
+var Hapi = require('hapi');
+var Path = require('path');
+
+var server = new Hapi.Server();
+server.connection({ port: 4000 });
+
+server.route({
+    method: 'GET',
+    path: '/image.png',
+    handler: function (request, reply) {
+
+        reply.file(Path.join(__dirname, 'image.png'));
+    },
+    config: {
+        cache: {
+            privacy: 'private',
+            expiresIn: 86400 * 1000
+        }
+    }
+});
+
+server.start(function () {
+
+    console.log('Server running at:', server.info.uri);
+});
