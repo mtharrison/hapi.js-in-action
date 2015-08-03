cae43a93c12f64bdc7fb1be9b24bbdd928b82d12
Matt Harrison <hi@matt-harrison.com>
2015-08-03 07:13:54 +0100

    Leading zero in chapter names to ensure correct ordering on Github

---
+++ CH05 - Understanding Requests and Responses /5.4/5.4.1/index.js
@@ -0,0 +1,19 @@
+// var Fs = require('fs');
+
+// Fs.readFile('/not/a/real/filepath', function (err, contents) {
+
+//     if (err) {
+//         console.log(err.name);
+//         console.log(err.message);
+//         console.log(JSON.stringify(err));
+
+//         throw err;
+//     }
+
+//     console.log(contents);
+// });
+
+var err = new Error('Woops, wasn’t supposed to do that!');
+
+console.log(err.name);
+console.log(err.message);""
---
+++ CH05 - Understanding Requests and Responses /5.4/5.4.3/index.js
@@ -0,0 +1,24 @@
+var Boom = require('boom');
+var Hapi = require('hapi');
+var Fs = require('fs');
+
+var server = new Hapi.Server();
+server.connection({ port: 4000 });
+
+server.route({
+    method: 'GET',
+    path: '/boom',
+    handler: function (request, reply) {
+        
+        Fs.readFile('/not/a/real/filepath', function (err) {
+
+            if (err) {
+                return reply(Boom.badRequest('Didn\'t something very bad'));
+            }
+
+            reply('ok');
+        });
+    }
+});
+
+server.start();
