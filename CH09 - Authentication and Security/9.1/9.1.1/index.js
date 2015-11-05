b2c468980f5e0acf03f120adc17cb9571aeaef78
Matt Harrison <hi@matt-harrison.com>
2015-11-05 21:45:50 +0000

    Connection

---
+++ CH09 - Authentication and Security/9.1/9.1.1/index.js
@@ -0,0 +1,44 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 4000 });
+
+var validUsers = {
+    john: 'secret',
+    jane: 'topsecret'
+};
+
+var validate = function (request, username, password, callback) {
+
+    var err = null;
+    var isValid = false
+    var credentials = {};
+
+    if (validUsers[username] && validUsers[username] === password) {
+        isValid = true;
+        credentials = { username: username };
+    }
+
+    callback(err, isValid, credentials);
+};
+
+server.register(require('hapi-auth-basic'), function (err) {
+
+    server.auth.strategy('simple', 'basic', { validateFunc: validate });
+    server.route({ 
+        method: 'GET', 
+        path: '/', 
+        config: { 
+            auth: 'simple',
+            handler: function (request, reply) {
+
+                reply('Hi ' + request.auth.credentials.username + '!' );
+            }
+        } 
+    });
+
+    server.start(function () {
+
+        console.log('Started server');
+    });
+});
---
+++ CH09 - Authentication and Security/9.4/connection/index.js
@@ -0,0 +1,39 @@
+var Hapi = require('hapi');
+var Path = require('path');
+
+var server = new Hapi.Server();
+server.connection({ 
+    port: 4000,
+    routes: {
+        cors: true
+    }
+});
+
+server.register(require('inert'), function (err) {
+
+    server.route([
+        {
+            method: 'GET',
+            path: '/',
+            handler: function (request, reply) {
+
+                reply.file(Path.join(__dirname, 'index.html'));
+            }
+        }, {
+            // config: {
+            //     cors: true
+            // },
+            method: 'GET',
+            path: '/resource',
+            handler: function (request, reply) {
+
+                reply('A resource');
+            }
+        }
+    ]);
+
+    server.start(function () {
+
+        console.log('Started server');
+    });
+});
