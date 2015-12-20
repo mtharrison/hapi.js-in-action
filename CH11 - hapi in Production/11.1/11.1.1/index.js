38d32f883300306ac0b050984f283b17224ce168
Matt Harrison <hi@matt-harrison.com>
2015-12-20 14:38:10 +0000

    Make linter happy

--- CH11 - hapi in Production/11.1/11.1.1/index.js
+++ CH11 - hapi in Production/11.1/11.1.1/index.js
@@ -1,9 +1,12 @@
+'use strict';
+
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 
 server.on('start', () => {
-    console.log('Server started!')
+
+    console.log('Server started!');
 });
 
 server.connection({ port: 4000 });
@@ -17,16 +20,7 @@ server.route({
     }
 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-
-        reply('hello');
-    }
-});
-
-server.start(err => {
+server.start((err) => {
 
     if (err) {
         throw err;
